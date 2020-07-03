4b1f9860c12fd073a8aa409506524290f2186d05
Morgan <morganlebeau53@gmail.com>
2020-07-03 14:59:27 +0200

    EspaceMembre form (not working)

--- src/views/EspaceMembre.vue
+++ src/views/EspaceMembre.vue
@@ -1,12 +1,50 @@
 <template>
     <div>
-        <h1>This is the member section</h1>
+        <h1>Rapport quotidien</h1>
+        <v-row>
+            <v-col cols="12" sm="4">
+                <h2>Physique</h2>
+                <v-text-field v-model="poids" type="number" label="Poids"></v-text-field>
+                <v-text-field v-model="sportPratique" label="Sport réalisé dans la journée"></v-text-field>
+                <v-text-field v-model="sportDuree" type="number" label="Durée"></v-text-field>
+                <v-select v-model="sportIntensite" :items="intensiteList" label="Intensité" ></v-select>
+            </v-col>
+            <v-col cols="12" sm="4">
+                <h2>Diététique</h2>
+                <v-select v-model="petitDejeuner" :items="repasList" label="Petit-déjeuner" ></v-select>
+                <v-select v-model="dejeuner" :items="repasList" label="Déjeuner" ></v-select>
+                <v-select v-model="diner" :items="repasList" label="Dîner" ></v-select>
+            </v-col>
+            <v-col cols="12" sm="4">
+                <h2>Psychologique</h2>
+                <v-select v-model="humeur" :items="humeurList" label="Humeur" ></v-select>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-spacer></v-spacer>
+            <v-btn color="success">Envoyer</v-btn>
+        </v-row>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return {
+                humeurList: ["Maussade", "Normal", "Bien", "Très bien"],
+                repasList: ["Léger", "Normal", "Conséquent"],
+                intensiteList: ["Faible", "Moyenne", "Forte"],
+                sportPratique: "",
+                sportDuree: "",
+                sportIntensite: "",
+                poids: "",
+                petitDejeuner: "",
+                dejeuner: "",
+                diner: "",
+                collation: false,
+                humeur: "",
+            }
+        },
     }
 </script>
 
