d44f98fde58a531da1bf11fa0b8f1db464993e79
Morgan <morganlebeau53@gmail.com>
2020-07-03 12:22:26 +0200

    Contact form (not working)

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,12 +1,48 @@
 <template>
     <div>
-        <h2>This is the contact component</h2>
+        <h2>Contact</h2>
+        <v-row>
+            <v-col cols="12" sm="3">
+                <v-text-field v-model="contactLastname" label="Nom" ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="3">
+                <v-text-field v-model="contactFirstname" label="Prénom" ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6">
+                <v-text-field v-model="contactEmail" label="Adresse email" ></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12" sm="6">
+                <v-text-field v-model="contactSubject" label="Sujet" ></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-textarea v-model="contactContent" label="Descriptif" ></v-textarea>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12" sm="10"></v-col>
+            <v-col cols="12" sm="1">
+                <v-btn color="success">Envoyer</v-btn>
+            </v-col>
+        </v-row>
+        <br>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return {
+                contactLastname: "",
+                contactFirstname: "",
+                contactEmail: "",
+                contactSubject: "",
+                contactContent: "",
+            }
+        },
     }
 </script>
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
       <Contact />
       <v-divider></v-divider>
       <LegalMentions />
